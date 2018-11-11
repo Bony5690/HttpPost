b25d3bc39df0344dc868cd681dc02de440759d88
Bony5690 <jerrybony@yahoo.com>
2018-11-11 13:22:38 -0600

    added routing
    Added routing to to tunr Single Plage App into multipage app.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,58 +1,27 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import {Route} from 'react-router-dom';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null
-  }
-
-
-  componentDidMount () {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-    .then(response => {
-      const posts = response.data.slice(0 ,4);
-      const updatedPosts = posts.map(post => {
-        return {
-          ...post,
-          author: 'Max'
-        }
-      });
-      this.setState({posts: updatedPosts});
-      //console.log(response);
-    });
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({selectedPostId: id});
-  }
-
     render () {
-     const posts = this.state.posts.map( post => {
-       return  <Post
-       clicked={() => this.postSelectedHandler(post.id)}
-       key={post.id}
-       title={post.title}
-       author={post.author} />
-
-     });
-
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
+          <div className="Blog">
+            <header>
+              <nav>
+                <ul>
+                  <li><a href="/">Home</a></li>
+                  <li><a href="/new-post">New Post</a></li>
+                </ul>
+              </nav>
+            </header>
+      {/* <Route path="/" exact render={()=> <Posts/>}/>
+      <Route path="/new-post"  render={()=> <NewPost/>}/> */}
+
+      <Route path="/" exact  component={Posts} />
+      <Route path="/new-post"  component={NewPost} />
+      </div>
         );
     }
 }
