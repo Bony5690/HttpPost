72b4f07fab350ddd4dc0744ce796a072292fe153
Bony5690 <jerrybony@yahoo.com>
2018-11-14 11:57:46 -0600

    more routing
    more routing and using Navlink and Link from react to keep navigation from sending out request and loading the page everytime

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
-import {Route} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 class Blog extends Component {
     render () {
@@ -11,8 +11,20 @@ class Blog extends Component {
             <header>
               <nav>
                 <ul>
-                  <li><a href="/">Home</a></li>
-                  <li><a href="/new-post">New Post</a></li>
+                  <li><NavLink 
+                  to='/' 
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: '#fa923f',
+                    textDecoration: 'underline'
+                  }}> Home</NavLink>
+                  </li>
+                  <li><NavLink to={{
+                    pathname: '/new-post',
+                    hash: '#submit',
+                    search: '?quick-submit=true'
+                  }}>New Post</NavLink></li>
                 </ul>
               </nav>
             </header>
