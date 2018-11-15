263017ea3c995f8217f5729faffc276dec092600
Bony5690 <jerrybony@yahoo.com>
2018-11-15 15:38:30 -0600

    added and extracted route parameters
    adding and extracting route parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import {Route, NavLink} from 'react-router-dom';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
@@ -28,11 +29,11 @@ class Blog extends Component {
                 </ul>
               </nav>
             </header>
-      {/* <Route path="/" exact render={()=> <Posts/>}/>
-      <Route path="/new-post"  render={()=> <NewPost/>}/> */}
+      
 
       <Route path="/" exact  component={Posts} />
       <Route path="/new-post"  component={NewPost} />
+      <Route path="/:id" exact  component={FullPost} />
       </div>
         );
     }
