c0bf29c5c95972392c5e370a9160770b9bbbfd7c
Bony5690 <jerrybony@yahoo.com>
2018-11-17 12:25:31 -0600

    conditional routing and redirects and nested routes
    conditional routing and redirects and nested routes

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,41 +1,43 @@
 import React, { Component } from 'react';
+// import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+
+import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
-import './Blog.css';
-import {Route, NavLink, Switch} from 'react-router-dom';
-import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render () {
         return (
-          <div className="Blog">
-            <header>
-              <nav>
-                <ul>
-                  <li><NavLink 
-                  to='/' 
-                  exact
-                  activeClassName="my-active"
-                  activeStyle={{
-                    color: '#fa923f',
-                    textDecoration: 'underline'
-                  }}> Home</NavLink>
-                  </li>
-                  <li><NavLink to={{
-                    pathname: '/new-post',
-                    hash: '#submit',
-                    search: '?quick-submit=true'
-                  }}>New Post</NavLink></li>
-                </ul>
-              </nav>
-            </header>
-            <Route path="/" exact  component={Posts} />
-    <Switch>
-      <Route path="/new-post"  component={NewPost} />
-      <Route path="/:id" exact  component={FullPost} />
-      </Switch>
-      
-      </div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/* <Route path="/" component={Posts} /> */}
+                </Switch>
+            </div>
         );
     }
 }
